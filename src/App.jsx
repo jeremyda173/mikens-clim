f1579b68976defc34ce9f22bb7145421854173c3
jeremyda173 <jeremygithud173@gmail.com>
2025-11-12 10:33:51 -0400

    feat: create main App component to render WeatherDashboard

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WeatherDashboard from './components/WeatherDashboard'
+import './App.css'
+
+function App() {
+  return (
+    <main className="app-shell">
+      <WeatherDashboard />
+    </main>
+  )
+}
+
+export default App
